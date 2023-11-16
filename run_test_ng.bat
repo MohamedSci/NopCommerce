395f9dc1a9b86a39428dcfeccd7e931dcea492eb
Mohamed Saeed <42535636+MohamedSci@users.noreply.github.com>
2023-11-16 12:22:15 +0200

    16-11-2023

---
+++ run_test_ng.bat
@@ -0,0 +1,3 @@
+set projectLocation=C:\Users\moham\OneDrive\Desktop\AAIB_Technical_challenge_updated\AAIB_Technical_challenge\3.NopCommerce\com.mohamedsaidsyed\testng.xml
+cd %projectLocation%
+mvn clean test -DxmlFilePath=src/main/java/testng.xml
