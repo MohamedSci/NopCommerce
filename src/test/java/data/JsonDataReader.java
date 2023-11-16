10155a70c9ec24b20813caea1df82b15d2ddc75e
Mohamed Saeed <42535636+MohamedSci@users.noreply.github.com>
2023-11-16 15:35:32 +0200

    16 11 2023 -json

--- src/test/java/data/JsonDataReader.java
+++ src/test/java/data/JsonDataReader.java
@@ -1,65 +1,45 @@
 package data;
 
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
-import com.google.gson.*;
-import com.google.gson.reflect.*;
-import org.apache.poi.ss.formula.functions.*;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 
-public class JsonDataReader 
+public class JsonDataReader
 {
+	public String firstname, lastname , email , password  ;
 
-	public Object[][] getData() throws FileNotFoundException, FileNotFoundException {
+	public void JsonReader() throws IOException, ParseException
+	{
 		String filePath = System.getProperty("user.dir")+"/src/test/java/data/UserData.json";
 
-		JsonElement jsonData =
-				new JsonParser().parse(new FileReader(filePath));
-		JsonElement dataSet = jsonData.getAsJsonObject().get("dataSet");
-		List<T> testData = new Gson().fromJson(dataSet, new TypeToken<List<T>>() {
-		}.getType());
-		Object[][] returnValue = new Object[testData.size()][1];
-		int index = 0;
-		for (Object[] each : returnValue) {
-			each[0] = testData.get(index++);
+		File srcFile = new File(filePath);
+
+		JSONParser parser = new JSONParser();
+		JSONArray jarray = (JSONArray)parser.parse(new FileReader(srcFile));
+
+		for(Object jsonObj : jarray)
+		{
+			JSONObject person = (JSONObject) jsonObj ;
+			firstname  = (String) person.get("firstname");
+			System.out.println(firstname);
+
+			lastname = (String) person.get("lastname");
+			System.out.println(lastname);
+
+			email = (String) person.get("email");
+			System.out.println(email);
+
+			password = (String) person.get("password");
+			System.out.println(password);
+
 		}
-		return returnValue;
+
 	}
-
-
-//	public String firstname, lastname , email , password  ;
-//
-//	public void JsonReader() throws IOException, ParseException
-//	{
-//		String filePath = System.getProperty("user.dir")+"/src/test/java/data/UserData.json";
-//
-//		File srcFile = new File(filePath);
-//
-//		JSONParser parser = new JSONParser();
-//		JSONArray jarray = (JSONArray)parser.parse(new FileReader(srcFile));
-//
-//		for(Object jsonObj : jarray)
-//		{
-//			JSONObject person = (JSONObject) jsonObj ;
-//			firstname  = (String) person.get("firstname");
-//			System.out.println(firstname);
-//
-//			lastname = (String) person.get("lastname");
-//			System.out.println(lastname);
-//
-//			email = (String) person.get("email");
-//			System.out.println(email);
-//
-//			password = (String) person.get("password");
-//			System.out.println(password);
-//
-//		}
-//
-//	}
 
 }
--- src/test/java/tests/UserRegistrationTestWithDDTAndJSON.java
+++ src/test/java/tests/UserRegistrationTestWithDDTAndJSON.java
@@ -4,53 +4,48 @@ package tests;
 
 import java.io.IOException;
 
-import data.*;
+import org.json.simple.parser.ParseException;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import data.JsonDataReader;
 import pages.HomePage;
 import pages.LoginPage;
 import pages.UserRegistrationPage;
 
 public class UserRegistrationTestWithDDTAndJSON extends TestBase
 {
+
 	HomePage homeObject ;
 	UserRegistrationPage registerObject ;
 	LoginPage loginObject ;
+	JsonDataReader jsonReader = new JsonDataReader();
 
-	@DataProvider(name="JSONData")
-	public Object[][] userRegisterData() throws IOException
+	@Test(priority=1,alwaysRun=true)
+	public void UserCanRegisterSuccssfullyJSON() throws IOException, ParseException
 	{
-		// get data from Excel Reader class
-		JsonDataReader JDR = new JsonDataReader();
-		return JDR.getData();
-	}
-
-	@Test(priority=1,alwaysRun=true,dataProvider="JSONData")
-	public void UserCanRegisterSuccssfully(String firstname , String lastname , String email , String password)
-	{
+		jsonReader.JsonReader();
 		homeObject = new HomePage(driver);
 		homeObject.openRegistrationPage();
 		registerObject = new UserRegistrationPage(driver);
-		registerObject.userRegistration(firstname,lastname,email,password);
+		registerObject.userRegistration(jsonReader.firstname,jsonReader.lastname,jsonReader.email,jsonReader.password);
 		Assert.assertTrue(registerObject.successMessage.getText().contains("Your registration completed"));
 	}
 
-
-	@Test(dependsOnMethods= {"RegisteredUserCanLogin"})
-	public void RegisteredUserCanLogout()
+	@Test(dependsOnMethods= {"RegisteredUserCanLoginJSON"})
+	public void RegisteredUserCanLogoutJSON()
 	{
 		registerObject.userLogout();
 	}
 
-	@Test(dependsOnMethods= {"UserCanRegisterSuccssfully"},dataProvider="JSONData")
-	public void RegisteredUserCanLogin(String email , String password)
-	{
+	@Test(dependsOnMethods= {"UserCanRegisterSuccssfullyJSON"})
+	public void RegisteredUserCanLoginJSON() throws IOException, ParseException {
+		jsonReader.JsonReader();
 		homeObject.openLoginPage();
 		loginObject = new LoginPage(driver);
-		loginObject.UserLogin(email, password);
+		loginObject.UserLogin(jsonReader.email, jsonReader.password);
 		Assert.assertTrue(registerObject.logoutLink.getText().contains("Log out"));
 	}
+
 
 }
