39e3727e6ff989d0f67044cfa37fa63fd0aa4001
Mohamed Saeed <42535636+MohamedSci@users.noreply.github.com>
2023-11-16 18:59:29 +0200

    16 11 2023 - Excel

--- src/test/java/tests/UserRegistrationTestWithDDTAndExcel.java
+++ src/test/java/tests/UserRegistrationTestWithDDTAndExcel.java
@@ -28,14 +28,14 @@ public class UserRegistrationTestWithDDTAndExcel extends TestBase
 	}
 
 	@Test(priority=1,alwaysRun=true,dataProvider="ExcelData")
-	public void UserCanRegisterSuccssfully(String firstname , String lastname , String email , String password)
+	public void UserCanRegisterSuccssfullyExcel(String firstname , String lastname , String email , String password)
 	{
 		homeObject = new HomePage(driver);
 		homeObject.openRegistrationPage();
 		registerObject = new UserRegistrationPage(driver);
 		registerObject.userRegistration(firstname,lastname,email,password);
 		Assert.assertTrue(registerObject.successMessage.getText().contains("Your registration completed"));
-		registerObject.userLogout();
+//		registerObject.userLogout();
 		homeObject.openLoginPage();
 		loginObject = new LoginPage(driver);
 		loginObject.UserLogin(email,password);
