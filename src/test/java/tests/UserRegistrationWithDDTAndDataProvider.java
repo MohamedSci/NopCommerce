4f6ff08c47cc7496f648c94358785ba784a39e3d
Mohamed Saeed <42535636+MohamedSci@users.noreply.github.com>
2023-11-16 18:14:52 +0200

    16 11 2023 - DATA PROVIDER

--- src/test/java/tests/UserRegistrationWithDDTAndDataProvider.java
+++ src/test/java/tests/UserRegistrationWithDDTAndDataProvider.java
@@ -2,6 +2,7 @@ package tests;
 
 
 
+import org.apache.log4j.*;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -21,8 +22,8 @@ public class UserRegistrationWithDDTAndDataProvider extends TestBase
 	public static Object[][] userData()
 	{
 		return new Object[][] {
-			{"Saz516" , "for1t660","test51660Saz00666@gmail.com","121345656"},
-			{"Ahmed1665","Sabe1h6660","test51606Sabeh00666@gmail.com","12341567856"}
+			{"Saz516" , "for1t660","test51660Saz9990@gmail.com","121345656"},
+			{"Ahmed1665","Sabe1h6660","test51606Sab0e999@gmail.com","12341567856"}
 		};
 	}
 
@@ -34,19 +35,12 @@ public class UserRegistrationWithDDTAndDataProvider extends TestBase
 		registerObject = new UserRegistrationPage(driver); 
 		registerObject.userRegistration(fname,lname,email,password);
 		Assert.assertTrue(registerObject.successMessage.getText().contains("Your registration completed"));
-	}
-	@Test(dependsOnMethods= {"RegisteredUserCanLoginDataProvider"})
-	public void RegisteredUserCanLogoutDataProvider()
-	{
 		registerObject.userLogout();
-	}
-
-	@Test(dependsOnMethods= {"UserCanRegisterSuccssfullyDataProvider"},dataProvider="testData")
-	public void RegisteredUserCanLoginDataProvider(String email , String password)
-	{
 		homeObject.openLoginPage();
 		loginObject = new LoginPage(driver);
 		loginObject.UserLogin(email,password);
 		Assert.assertTrue(registerObject.logoutLink.getText().contains("Log out"));
+		registerObject.userLogout();
 	}
+
 }
